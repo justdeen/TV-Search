67ef73a880371c5bb630c85480af0aa835c65f3a
Sheriff Aliu Yamah <112347873+justdeen@users.noreply.github.com>
2024-11-12 21:08:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ let error = 0
 // Eventlistener for storing the response from the API call
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
-    let children = Array.from(grid.children)
-    children.forEach(elem => elem.remove())
+    //let children = Array.from(grid.children)
+    //children.forEach(elem => elem.remove())
     let searchTerm = field.value
-    let config = {params: {q: searchTerm}}
+    //let config = {params: {q: searchTerm}}
     //res = await axios.get(`http://api.tvmaze.com/search/shows?`, config)
     field.value = ''
     document.body.style.backgroundColor = "brown"
